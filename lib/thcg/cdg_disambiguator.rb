f4eb4b19f768d19606395e7e8656f716058edeea
Christian Rishøj <christian@rishoj.net>
2011-04-04 16:24:35 +0700

    * count some things (to estimate the impact of changes)  * strip leading/trailing doube quotes from word forms

--- lib/thcg/cdg_disambiguator.rb
+++ lib/thcg/cdg_disambiguator.rb
@@ -16,24 +16,24 @@ if __FILE__ == $PROGRAM_NAME
   cdg_file = ARGV.shift  
   dep_file = ARGV.shift
   req_sent_no = ARGV.shift.to_i
-  sent_no = 0
   
   converter = THCG::CDGConverter.new
 
   cdg_sents = File.new(cdg_file).each("\r\n\r\n")
   dep_sents = File.new(dep_file).each("\n\n")
 
+  counters = Hash.new(0)
   
   dep_tree = Conll::Sentence.parse(dep_sents.next.split("\n"))
   while (cdg_sent = cdg_sents.next)
-    sent_no += 1
-    next if req_sent_no > 0 and sent_no != req_sent_no
+    counters[:sentences] += 1
+    next if req_sent_no > 0 and counters[:sentences] != req_sent_no
     
     cdg_parses = cdg_sent.chomp.split(/\r?\n/)
     unparsed = cdg_parses.shift
-    cdg_forms = unparsed.split('|').compact
+    cdg_forms = unparsed.split('|').compact.collect { |form| form.gsub(/\A"|"\Z/, '') }
 
-    warn "Sentence #{sent_no}: #{unparsed}, #{cdg_parses.count} CDG parses\n\tCDG forms: #{cdg_forms}\n\tDep forms: #{dep_tree.tokens.forms}"
+    warn "Sentence #{counters[:sentences]}: #{unparsed}, #{cdg_parses.count} CDG parses\n\tCDG forms: #{cdg_forms}\n\tDep forms: #{dep_tree.tokens.forms}"
     
     if cdg_forms != dep_tree.tokens.forms
       warn "Forms mismatch. Skipping (assuming missing dependency tree)"
@@ -57,8 +57,14 @@ if __FILE__ == $PROGRAM_NAME
       end
     end
     warn "#{compatible.count} compatible CDG parses"
+    if compatible.count > 0
+      counters[:compatible] += 1
+      counters[:ambiguous] += 1 if compatible.count > 1
+    else
+      counters[:incompatible] += 1
+    end
     puts compatible.join("\n")
-#    break
+    warn "Counts: #{counters.inspect}"
     dep_tree = Conll::Sentence.parse(dep_sents.next.split("\n"))
   end
 end
